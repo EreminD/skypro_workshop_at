568be3dad52215ecc8ff8916d1772855792711c4
BeHuAMuH1991 <torhovaleksej@gmail.com>
2023-03-30 20:54:17 +0300

    Cоздал тесты

--- test/add_task_test.py
+++ test/add_task_test.py
@@ -7,3 +7,20 @@ def test_add():
     
     assert response.status_code == 201
     assert response_body['completed'] == False
+
+
+def test_create_1():
+    body = {"title":"generated123"}
+    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+    response_body = response.json()
+
+    assert response_body['title'] == 'generated123'
+
+
+def test_create_2():
+    body = {"title":"generated", "completed":False}
+    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+    response_body = response.json()
+
+    assert response_body['title'] == 'generated'
+    assert response_body['completed'] == False
